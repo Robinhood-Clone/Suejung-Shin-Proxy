738fb02423fb32a13490a635c3376e68adbf5287
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2019-12-11 17:47:33 -0800

    Clean up code after connecting

--- server.js
+++ server.js
@@ -10,22 +10,3 @@ app.use('/stocks/:ticker', express.static(__dirname)); // serves the index.html 
 app.listen(port, ()=> {
   console.log(`Hello, I am listening on port ${port}`);
 });
-
-
-//app.use('/', express.static(path.join(__dirname)));
-
-
-// getAnalystRatings(stockTicker, callback) {
-//   $.ajax({
-//     method: 'GET',
-//     url: `http://localhost:4000/api/analystRatings/?ticker=${stockTicker}`,
-//     success: (data) => {
-//       this.setState({
-//         currentStockTicker: stockTicker,
-//         details: data[0]
-//       });
-//       console.log('Get request successful: ', data);
-//       console.log('State after get request: ', this.state);
-//     }
-//   });
-// }
