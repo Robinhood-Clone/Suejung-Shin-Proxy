cc7f51fc47d4ec4621d769bd2edaab2e245caa9a
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2019-12-11 17:35:18 -0800

    Connect four modules

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ const port = 2000;
 const cors = require('cors');
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname)));
 app.use(cors());
+app.use('/stocks/:ticker', express.static(__dirname)); // serves the index.html from the current directory
 
 app.listen(port, ()=> {
   console.log(`Hello, I am listening on port ${port}`);
 });
 
 
+//app.use('/', express.static(path.join(__dirname)));
+
+
+// getAnalystRatings(stockTicker, callback) {
+//   $.ajax({
+//     method: 'GET',
+//     url: `http://localhost:4000/api/analystRatings/?ticker=${stockTicker}`,
+//     success: (data) => {
+//       this.setState({
+//         currentStockTicker: stockTicker,
+//         details: data[0]
+//       });
+//       console.log('Get request successful: ', data);
+//       console.log('State after get request: ', this.state);
+//     }
+//   });
+// }
