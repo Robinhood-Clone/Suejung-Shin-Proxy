9aff3b69d915c2a05e6f61f3a82e366f43778302
Suejung Shin <suejungshin@Suejungs-MBP.attlocal.net>
2019-12-15 23:29:17 -0800

    Update so proxy displays combined modules

--- server.js
+++ server.js
@@ -10,3 +10,12 @@ app.use('/stocks/:ticker', express.static(__dirname)); // serves the index.html 
 app.listen(port, ()=> {
   console.log(`Hello, I am listening on port ${port}`);
 });
+
+// app.get('/api/earnings', (req, res, next) => {
+//   res.redirect('http://18.224.6.69/api/earnings');
+// });
+
+// app.get('/api/analystRatings', (req, res, next) => {
+//   res.redirect('http://3.136.112.156/api/analystRatings');
+// });
+
